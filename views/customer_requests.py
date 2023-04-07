ef4120de27f796dfb6a35cef2f21550f16a601cf
Angie Gonzalez <angiem.go@gmail.com>
2023-04-07 16:57:32 -0500

    successful PUT replaces whole resource

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -46,3 +46,11 @@ def delete_customer(id):
             customer_index = index
     if customer_index >= 0:
         CUSTOMERS.pop(customer_index)
+
+def update_customer(id, new_customer):
+    """POST will replace whole resource with UPDATE
+    """
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            CUSTOMERS[index] = new_customer
+            break
--- views/employee_requests.py
+++ views/employee_requests.py
@@ -45,3 +45,11 @@ def delete_employee(id):
             employee_index = index
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
+
+def update_employee(id, new_employee):
+    """POST will replace whole resource with UPDATE
+    """
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            EMPLOYEES[index] = new_employee
+            break
